c74a42a5a28c717e2ae628741e1228d901488e9a
Helloyunho <yunho050840@gmail.com>
2022-10-10 15:16:42 +0900

    ⚰️ chore: (finally) completely remove vscode related things

--- src/lang.ts
+++ src/lang.ts
@@ -1,61 +1,52 @@
 import { AFFLexer } from './lexer'
 import { affParser } from './parser'
-import { EOF } from 'chevrotain'
 import { affToAST } from './to-ast'
-import { AFFError } from './types'
+import { AFFError, AFFErrorLevel } from './types'
 import { processCheckers } from './checkers'
 
-export const checkAFF = (content: TextDocument): lsp.Diagnostic[] => {
-  let errors: lsp.Diagnostic[] = []
-  const text = content.getText()
-  const lexingResult = AFFLexer.tokenize(text)
+export const checkAFF = (content: string): AFFError[] => {
+  const lexingResult = AFFLexer.tokenize(content)
+  let errors: AFFError[] = []
   if (lexingResult.errors.length > 0) {
     errors = errors.concat(
       lexingResult.errors.map((e) => ({
-        severity: lsp.DiagnosticSeverity.Error,
+        severity: AFFErrorLevel.Error,
         message: e.message,
-        range: {
-          start: { line: e.line - 1, character: e.column - 1 },
-          end: content.positionAt(
-            content.offsetAt({ line: e.line - 1, character: e.column - 1 }) +
-              e.length
-          )
+        location: {
+          startOffset: e.offset,
+          startLine: e.line,
+          startColumn: e.column,
+          endOffset: e.offset + e.length,
+          endLine: e.line,
+          endColumn: e.column + e.length
         }
       }))
     )
   }
+
   // The error tokens is just ignored so we can find more errors in parsing stage
   affParser.input = lexingResult.tokens
   const parsingResult = affParser.aff()
   if (affParser.errors.length > 0) {
     errors = errors.concat(
       affParser.errors.map((e) => ({
-        severity: lsp.DiagnosticSeverity.Error,
+        severity: AFFErrorLevel.Error,
         message: e.message,
-        range:
-          e.token.tokenType === EOF
-            ? {
-                start: content.positionAt(text.length),
-                end: content.positionAt(text.length)
-              }
-            : {
-                start: {
-                  line: e.token.startLine - 1,
-                  character: e.token.startColumn - 1
-                },
-                end: { line: e.token.endLine - 1, character: e.token.endColumn }
-              }
+        location: {
+          startOffset: e.token.startOffset,
+          startLine: e.token.startLine,
+          startColumn: e.token.startColumn,
+          endOffset: e.token.endOffset,
+          endLine: e.token.endLine,
+          endColumn: e.token.endColumn
+        }
       }))
     )
   } else {
     const astResult = affToAST(parsingResult)
-    errors = errors.concat(
-      astResult.errors.map((e) => transformAFFError(e, content.uri))
-    )
+    errors = errors.concat(astResult.errors)
     const checkerErrors = processCheckers(astResult.ast)
-    errors = errors.concat(
-      checkerErrors.map((e) => transformAFFError(e, content.uri))
-    )
+    errors = errors.concat(checkerErrors)
   }
   return errors
 }
